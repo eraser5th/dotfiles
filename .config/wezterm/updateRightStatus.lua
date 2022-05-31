cdc5c251412d9eefe5c93c618ca671ead8f33371
eraser5th <hisanniwo0202@gmail.com>
2022-05-31 21:19:40 +0900

    split updateRightStatus setting

--- .config/wezterm/updateRightStatus.lua
+++ .config/wezterm/updateRightStatus.lua
@@ -1,49 +1,38 @@
 local wezterm = require 'wezterm';
 local util = require 'util';
+local icons = require('icons')
 
-local BATTERY_0 = utf8.char(0xf244)
-local BATTERY_1 = utf8.char(0xf243)
-local BATTERY_2 = utf8.char(0xf242)
-local BATTERY_3 = utf8.char(0xf241)
-local BATTERY_4 = utf8.char(0xf240)
-local CLOCK = utf8.char(0xf64f)
-local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+local function create_cwd_cell(pane)
+  local cwd_uri = pane:get_current_working_dir()
+  if cwd_uri then
+    return cwd_uri:sub(8);
+  end
+  return "cannot get cwd uri"
+end
+
+local function create_date_cell()
+  return icons.CLOCK
+    .. " "
+    .. wezterm.strftime("%a %b %-d %H:%M:%S");
+end
+
+local function create_battery_cell()
+  local battery_info = wezterm.battery_info()[1]
+  local battery_percentage = battery_info.state_of_charge * 100
+  local is_charging = battery_info.state == 'Charging'
+
+  local BATTERY = icons.fn.BATTERY(
+    battery_percentage,
+    is_charging
+  )
+
+  return BATTERY .. string.format("  %.0f%%", battery_percentage)
+end
 
 return function(window, pane)
   local cells = {};
 
-  -- working dir info
-  local cwd_uri = pane:get_current_working_dir()
-  if cwd_uri then
-    -- delete 'file: //'
-    cwd_uri = cwd_uri:sub(8);
-    table.insert(cells, cwd_uri);
-  end
-
-  -- date info
-  local date = wezterm.strftime("%a %b %-d %H:%M:%S");
-  table.insert(cells, CLOCK .. " " .. date);
-
-  -- battery info
-  local state_of_charge = wezterm.battery_info()[1].state_of_charge * 100
-
-  local BATTERY = ""
-
-  if state_of_charge <= 10 then
-    BATTERY = BATTERY_0
-  elseif state_of_charge <= 25 then
-    BATTERY = BATTERY_1
-  elseif state_of_charge <= 50 then
-    BATTERY = BATTERY_2
-  elseif state_of_charge <= 75 then
-    BATTERY = BATTERY_3
-  else
-    BATTERY = BATTERY_4
-  end
-
-  table.insert(cells, BATTERY .. string.format("  %.0f%%", state_of_charge))
-
-
+  local text_fg = "#c0c0c0";
   local colors = {
     "#3c1361",
     "#52307c",
@@ -52,7 +41,9 @@ return function(window, pane)
     "#b491c8",
   };
 
-  local text_fg = "#c0c0c0";
+  table.insert(cells, create_cwd_cell(pane))
+  table.insert(cells, create_date_cell());
+  table.insert(cells, create_battery_cell())
 
   local elements = {};
   local num_cells = 0;
@@ -60,7 +51,7 @@ return function(window, pane)
   local function push(text, _)
     local cell_no = num_cells + 1
     table.insert(elements, {Foreground={Color=colors[cell_no]}})
-    table.insert(elements, {Text=SOLID_LEFT_ARROW})
+    table.insert(elements, {Text=icons.SOLID_LEFT_ARROW})
     table.insert(elements, {Foreground={Color=text_fg}})
     table.insert(elements, {Background={Color=colors[cell_no]}})
     table.insert(elements, {Text=" "..text.." "})
