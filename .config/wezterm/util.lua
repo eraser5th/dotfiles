485648d1b7adbd2381f34d97e355827fa6cd9f0b
eraser5th <hisanniwo0202@gmail.com>
2022-06-01 00:15:35 +0900

    update util

--- .config/wezterm/util.lua
+++ .config/wezterm/util.lua
@@ -1,19 +1,17 @@
-local util = {}
-
 --- @param key string
 --- @param mods string
 --- @param action any
-util.createKeyBind = function (key, mods, action)
+local function createKeyBind (key, mods, action)
   return {key=key, mods=mods, action=action}
 end
 
-util.forEach = function (target, callback)
+local function forEach (target, callback)
   for key, value in pairs(target) do
     callback(value, key)
   end
 end
 
-util.map = function (target, callback)
+local function map (target, callback)
   local result = {}
   for key, value in pairs(target) do
     result[key] = callback(value, key)
@@ -21,4 +19,66 @@ util.map = function (target, callback)
   return result
 end
 
-return util
+local function flat (input, level)
+  local result = {}
+  if level == nil then
+    level = 1
+  end
+
+  local function inner_flat(inner_input, current_level)
+    forEach(inner_input, function(v, _)
+      if current_level == 0 then
+        table.insert(result, v)
+      elseif type(v) == "table" then
+        inner_flat(v, current_level - 1)
+      else
+        table.insert(result, v)
+      end
+    end)
+  end
+  inner_flat(input, level)
+
+  return result
+end
+
+local function nest_blank(n)
+  local result = ""
+  local i = 0
+  while i < n do
+    result = result .. "  "
+    i = i + 1
+  end
+  return result
+end
+
+
+local function table_to_string (input)
+  local result = "{\n"
+
+  local function inner_table_to_string(inner_input, nest_level)
+    forEach(inner_input, function(v, k)
+      if type(v) == "table" then
+        result = result
+          .. nest_blank(nest_level)
+          .. k
+          .. ": "
+          .. inner_table_to_string(v, nest_level + 1)
+          .. "\n"
+      else
+        result = result .. k .. ": " .. v .. "\n"
+      end
+    end)
+  end
+
+  inner_table_to_string(input, 0)
+  result = result .. "}"
+  return result
+end
+
+return {
+  createKeyBind = createKeyBind,
+  forEach = forEach,
+  map = map,
+  flat = flat,
+  table_to_string = table_to_string
+}
