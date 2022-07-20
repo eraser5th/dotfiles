8f85aefc367761a926957d03c98d000f7dc23554
eraser5th <83107074+eraser5th@users.noreply.github.com>
2022-07-20 17:38:12 +0900

    add keymap stylish

--- .config/nvim/lua/plugins/layout/stylish.lua
+++ .config/nvim/lua/plugins/layout/stylish.lua
@@ -1,17 +1,24 @@
 vim.cmd [[
-amenu Plugin.Fugitive.GFetch :GFetch | amenu Plugin.Fugitive.GPull :GPull | amenu Plugin.Fugitive.GPush :GPush
-amenu File.Filetype.One :echo 1 | amenu File.Filetype.Two :echo 2 | amenu File.Filetype.Three :echo 3
-amenu Edit.Recent.Foo :echo 'foo' | amenu Edit.Recent.Bar :echo 'bar' | amenu Edit.Recent.Baz :echo 'baz'
-amenu Edit.Diff.Revision_1 :echo 'rev_1' | amenu Edit.Diff.Revision_2 :echo 'rev_2' | amenu Edit.Diff.Revision_3 :echo 'rev_3'
+amenu Plugin.Fugitive.GFetch :GFetch      | amenu Plugin.Fugitive.GPull :GPull        | amenu Plugin.Fugitive.GPush :GPush
+amenu File.Filetype.One :echo 1           | amenu File.Filetype.Two :echo 2           | amenu File.Filetype.Three :echo 3
+amenu Edit.Recent.Foo :echo 'foo'         | amenu Edit.Recent.Bar :echo 'bar'         | amenu Edit.Recent.Baz :echo 'baz'
+amenu Edit.Diff.Revision_1 :echo 'rev_1'  | amenu Edit.Diff.Revision_2 :echo 'rev_2'  | amenu Edit.Diff.Revision_3 :echo 'rev_3'
 ]]
 
 for i = 1, 16 do
   vim.cmd('amenu OverflowList.Test_Thing_' .. i .. ' :echo ' .. i)
 end
 
-vim.keymap.set(
+vim.api.nvim_set_keymap(
   'n',
   '<F12>',
-  require"stylish".ui_clock,
+  '<Cmd>lua require"stylish".ui_clock()<CR>',
+  { noremap = true, silent = true }
+)
+
+vim.api.nvim_set_keymap(
+  'n',
+  '<F1>',
+  "<Cmd>lua require'stylish'.ui_menu(vim.fn.menu_get(''), {kind=menu, prompt = 'Main Menu', experimental_mouse = true}, function(res) print('### ' ..res) end)<CR>",
   { noremap = true, silent = true }
 )
