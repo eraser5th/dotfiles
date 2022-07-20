00b5040014058cb4aaa772433100b70d5e927554
eraser5th <83107074+eraser5th@users.noreply.github.com>
2022-07-20 17:55:26 +0900

    tab barがnvim-treeの文空白を開けるプログラムがエラーを出すので消去。TODO: buffer-lineからset_offsetが消えたかどうかを確認する

--- .config/nvim/lua/plugins/layout/barbar.lua
+++ .config/nvim/lua/plugins/layout/barbar.lua
@@ -66,23 +66,3 @@ require'bufferline'.setup({
   no_name_title = nil,
 })
 
-local nvim_tree_events = require('nvim-tree.events')
-local bufferline_state = require('bufferline.state')
-
-local function get_tree_size()
-  if vim.bo.filetype == 'NvimTree' then
-    return vim.api.nvim_win_get_height(0)
-  end
-  return 0
-end
-nvim_tree_events.on_tree_open(function()
-  bufferline_state.set_offset(get_tree_size())
-end)
-
-nvim_tree_events.on_tree_resize(function()
-  bufferline_state.set_offset(get_tree_size())
-end)
-
-nvim_tree_events.on_tree_close(function()
-  bufferline_state.set_offset(0)
-end)
